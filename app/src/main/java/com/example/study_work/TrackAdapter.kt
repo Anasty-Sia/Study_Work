22b1ef2e81b0ee02f151143a1d0ddcaa6fe07a82
Nastya <anastasiasizonenkoo@yandex.ru>
2026-05-13 12:33:08 +0300

    Продвинутый UI: списки и библиотеки

---
+++ app/src/main/java/com/example/study_work/TrackAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.study_work
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class TrackAdapter(
+    private var tracks: List<Track>
+) : RecyclerView.Adapter<TrackViewHorder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHorder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.track_view, parent, false)
+        return TrackViewHorder(view)
+    }
+
+    override fun onBindViewHolder(holder: TrackViewHorder, position: Int) {
+        holder.bind(tracks[position])
+    }
+
+    override fun getItemCount(): Int {
+        return tracks.size
+    }
+
+   fun updateTracks(newTrack: List<Track>) {
+        tracks = newTrack
+        notifyDataSetChanged()
+    }
+}
